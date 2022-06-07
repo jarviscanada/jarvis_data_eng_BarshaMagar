849193e772c0c9537972d563dc43317826bbdc32
Barsha Layo Magar <barsamagar18@gmail.com>
2022-06-07 21:37:56 +0000

    update

--- linux_sql/scripts/ddl.sql
+++ linux_sql/scripts/ddl.sql
@@ -15,7 +15,14 @@ CREATE TABLE PUBLIC.host_info
      -- add more columns
      -- primary key constraint
      -- unique hostname constraint
-  );
+  CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
+    $$
+    BEGIN
+        RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+    END;
+    $$
+        LANGUAGE PLPGSQL;
+    );
 
 -- DML
 -- INSERT statement
--- linux_sql/scripts/resusg.sql
+++ linux_sql/scripts/resusg.sql
@@ -1,18 +1,18 @@
 CREATE TABLE PUBLIC.host_usage
   (
-     timestamp    TIMESTAMP NOT NULL,
      host_id        SERIAL NOT NULL,
-memory_free int not null,
-cpu_idle varchar not null,
-cpu_kernel int not null,
-disk_io int not null,
-disk_available int not null
+     memory_free int not null,
+     cpu_idle varchar not null,
+     cpu_kernel varchar not null,
+     disk_io varchar not null,
+     disk_available varchar not null,
+     timestamp    varchar NOT NULL
      -- add more columns
      -- add foreign key constraint
   );
 
 -- DML
 -- INSERT statement
-INSERT INTO host_usage (timestamp, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+INSERT INTO host_usage (memory_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp)
 values
-('2019-05-29 16:53:28',256,'95',0,0,31220);
+(256,'95','0','0','31220','2019-05-29 16:53:28');
