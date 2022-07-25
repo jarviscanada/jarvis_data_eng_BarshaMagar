d0e11836d8d1a6194dba273659ee1d6650b7f5f8
BarshaMgr <barsamagar18@gmail.com>
2022-07-25 18:53:21 -0400

    Update queries.sql

--- linux_sql/SQL/queries.sql
+++ linux_sql/SQL/queries.sql
@@ -1,11 +1,12 @@
-## Group hosts by CPU number and sort by their memory size in descending order
+--Group hosts by CPU number and sort by their memory size in descending order
 
 select id, cpu_number, total_mem
 from host_info
-GROUP BY cpu_number, id, total_mem ORDER BY cpu_number ASC, total_mem desc
+GROUP BY cpu_number, id, total_mem 
+ORDER BY cpu_number ASC, total_mem desc
 
 
-## Average used memory in percentage over 5 mins interval for each host.
+--Average used memory in percentage over 5 mins interval for each host.
  CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
 $$
 BEGIN
@@ -18,3 +19,9 @@ select host_id, hostname, round5(host_usage.timestamp) as rounded_timestamp ,  (
 from host_usage
 inner join host_info on host_usage.host_id = host_info.id
 group by rounded_timestamp, host_id, hostname, total_mem, memory_free;
+
+--Detect host failure
+select host_id, round5(timestamp) as ts, count(timestamp) as num_of_hostfailure
+from host_usage
+group by host_id, ts
+having count(timestamp) < 3
