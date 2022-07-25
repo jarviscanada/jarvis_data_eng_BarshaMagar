b31f2b939ba409b792dd4c0f91591e20f7fec55e
BarshaMgr <barsamagar18@gmail.com>
2022-07-25 19:02:39 -0400

    Update psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,29 +1,69 @@
-#!/bin/bash
+#! /bin/sh
 
-#save number of CPU to a variable
-lscpu_out=`lscpu`
-meminfo_out=`cat /proc/meminfo`
-#note: `xargs` is a trick to remove leading and trailing white spaces
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+#capture CLI arguments
+cmd=$1
+db_username=$2
+db_password=$3
 
+#Start docker
+sudo systemctl status docker || sudo systemctl start docker 
+echo 'docker started'
 
-#hardware
-hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out"  | egrep "Architecture" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | sed -n 's/Model name:[ \t]*//p' | awk '{print $2}' | xargs)
-cpu_mhz=$(echo "$lscpu_out"  | sed -n 's/CPU MHz:[ \t]*//p' | awk '{print $2}' | xargs)
-l2_cache=$(echo "$lscpu_out"  | sed -n 's/L2 Cache:[ \t]*//p' | awk '{print $2}' | xargs)
-total_mem=
-timestamp= #current timestamp in `2019-11-26 14:40:19` format
-echo $cpu_architecture
-#usage
-memory_free=$(echo "$meminfo_out"  | egrep "MemFree" | awk '{print $2}' | xargs)
-cpu_idle=
-cpu_kernel=
-disk_io=
-disk_available=
+#check container status
+docker container inspect jrvs-psql 
+container_status=$?
+echo 'container status = '$container_status
 
+#User switch case to handle create|stop|start options
+case $cmd in
+  create)
+  # Check if the container is already created
+  if [ $container_status -eq 0 ]; then
+		echo 'Container already exists'
+		exit 1
+	fi
 
-echo $memory_free
+	#check # of CLI arguments
+  if [ $# -ne 3 ]; then
+    echo 'Create requires username and password'
+    exit 1
+  fi
 
+  #Create container
+	  docker volume create pgdata
+	  export PGPASSWORD='password'
+	  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+	  echo 'container created'
+	  exit 0
+	;;
+
+  start)
+ # check instance status; start container if it has been created, if not exit 1
+  if [ $container_status -eq 0 ]; then
+    docker container start jrvs-psql
+    echo 'started container'
+    exit 0
+  else
+    echo 'container has not been created'
+    	  exit 1
+	fi
+	;;
+
+  stop)
+    # check status; if container exists, stop container, otherwise exit 1
+    if [ $container_status -eq 0 ]; then
+      docker container stop jrvs-psql
+      echo 'container stopped'
+      exit 0
+      else
+        echo 'container has not been created'
+        	  exit 1
+    fi
+    ;;
+
+  *)
+	  echo 'Illegal command'
+	  echo 'Commands: start|stop|create'
+	  exit 1
+	  ;;
+esac
